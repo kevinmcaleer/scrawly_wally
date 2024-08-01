5772f6e5001ed8c9da9360a9b5a391673cdaad7d
Kevin McAleer <kevinmcaleer@gmail.com>
2024-08-01 20:57:00 +0100

    Added scrawly class

--- scrawly.py
+++ scrawly.py
@@ -1,2 +1,17 @@
 # Scrawly Wally
 # A Wall drawing robot
+
+class Scrawly():
+
+    def move_xy(self, x:int, y:int):
+        # Moves the gondola to the X & Y position
+        pass
+
+    def pen_up(self):
+        # Moves the pen off the drawing surface
+        pass
+
+    def pen_down(self):
+        # Places the pen on the drawing surface
+        pass
+
